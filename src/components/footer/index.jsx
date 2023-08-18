a0f59ebfb7590c6c425d062c987343de6083dfde
ramazangalip <ramazansaidgalip@gmail.com>
2023-08-18 18:55:58 +0300

    footer added

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -22,29 +22,20 @@ const Footer = () => {
         >
             <div className="footer-content-wrapper">
                 <div className="footer-logo">
-                    <img
-                        src={Logo}
-                        alt="JS Dev Hindi"
-                    />
+                    
                 </div>
                 <ul className="footer-menu-items">
                     <li
                         className="footer-menu-item"
                         onClick={() => scrollToSection("skills")}
                     >
-                        Skills
+                        Yetenekler
                     </li>
                     <li
                         className="footer-menu-item"
                         onClick={() => scrollToSection("portfolio")}
                     >
-                        Portfolio
-                    </li>
-                    <li
-                        className="footer-menu-item"
-                        onClick={() => scrollToSection("blogs")}
-                    >
-                        Blogs & Articles
+                        Portfolyom
                     </li>
                     <li
                         className="footer-menu-item"
@@ -52,35 +43,30 @@ const Footer = () => {
                             scrollToSection("contact");
                         }}
                     >
-                        Contact me
+                        İletişim
                     </li>
                 </ul>
                 <div className="social-icons">
                     <SocialIcon
-                        color="#FF0000"
-                        icon={<FaYoutube />}
-                        link="https://www.youtube.com/"
-                    />
-                    <SocialIcon
                         color="#0D2636"
                         icon={<FaGithub />}
+                        link="https://github.com/ramazangalip"
                     />
                     <SocialIcon
                         color="#0A66C2"
                         icon={<FaLinkedinIn />}
+                        link="https://www.linkedin.com/in/ramazan-said-galip-330956243/"
                     />
-                    <SocialIcon
-                        color="#f2740d"
-                        icon={<FaStackOverflow />}
-                    />
+                    
                     <SocialIcon
                         color="#E84C88"
                         icon={<FaInstagram />}
+                        link="https://www.instagram.com/ramazangalip14/"
                     />
                 </div>
                 <div className="bottom-bar">
                     <div className="copyright-text">
-                        Copyright 2022 JS Dev Hindi | All Rights Reserved
+                        tüm hakları saklıdır 
                     </div>
                 </div>
             </div>
